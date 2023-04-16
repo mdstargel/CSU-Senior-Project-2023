2ed646289c75fffc7d57c55ed66f0bc73f310d27
mdstargel <mdstargel@csustudent.net>
2023-04-16 20:03:03 +0000

    Add account activation w/o SendGrid finished

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "mdstargel@csustudent.net"
   layout "mailer"
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -79,11 +79,11 @@ class LogoutTest < Logout
   end
 end
 
-  class RememberingTest < UsersLogin
+class RememberingTest < UsersLogin
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_equal cookies['remember_token'], assigns(:user).remember_token
+    assert_not cookies[:remember_token].blank?
   end
 
   test "login without remembering" do
